96e1c0d54f5c3c116832a2ee24472d1ae4f11415
Saeed Mafipour <m.saeed.mafipour@tum.de>
2025-02-11 12:11:09 +0100

    first commit

--- components/footer/SocialMediaLinks.tsx
+++ components/footer/SocialMediaLinks.tsx
@@ -5,7 +5,7 @@ const SocialMediaLinks = () => {
     <div className="pt-4 md:pr-8 md:pt-0 flex gap-6 items-center md:self-start justify-center">
       {socialLinks.map(({ href, icon: Icon }) => (
         <a key={href} href={href} target="_blank" rel="noopener noreferrer">
-          <Icon className="w-8 h-8 text-stone-300" />
+          <Icon className="w-8 h-8 text-stone-200" />
         </a>
       ))}
     </div>
